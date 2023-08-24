8bace9f6bbdcc635ebcd0e8fda84193e87a23f23
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-24 14:09:11 -0600

    Added new element that displays whether you won or lost.

--- app.js
+++ app.js
@@ -22,19 +22,42 @@ const winningPositions = [
     [1, 5, 9],
     [3, 5, 7]
 ];
+
 console.table(winningPositions)
 
 console.log(currentPlays)
 
+// Create a function that creates a message.
+function displayMessage(message) {
+    // New div that contains the message "You Won!" or "You Loose!"
+    const messageDiv = $('<div></div>')
+        .addClass('fixed inset-0 flex items center justify-center z-10 w-2/12 h-auto bg-teagreen text-2xl text-black')
+        .text(message);
+
+    $('body').append(messageDiv);
+
+    //Blur the main content
+    $('main').addClass('blur');
+}
+
+
 // document object is selected
 // .ready() is a method that is loaded before the website is displayed to the user.
 $(document).ready(function () {
+    // Tracks moves of each player
+    let moves = { 'X': [], 'O': [] }; 
     // Select the divs with class="cell"
     $('.cell').on('click', function () {
         // this refers to the divs with class="cell" 
         // .text() method returns the selected element and all of it's decendents as a string. 
         $(this).text(currentPlayer);
-
+        // Records the move...
+        moves[currentPlayer].push(parseInt($(this).attr('data-index')))
+        // Toggles current player with ternary operator
         currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+
+        if (isWinner) {
+            document.attr('div')
+        }
     })
 })
