3924c1b3ed348283cc3893077840df1a79b27fa8
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-24 11:33:32 -0600

    Added .on('click') functionality to clicking each cell and inputing 'X' or 'O'.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const board = $('#board');
 let currentPlayer = "X";
 let activeGame = true;
+
+/*
+    Select the document object, use the .ready() method to select each cell, and add an eventlisener of click.
+*/
+
+$(document).ready(function () {
+    // Select the divs with id="cell"
+    $('.cell').on('click', function () {
+        $(this).text(currentPlayer);
+
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+    })
+})
