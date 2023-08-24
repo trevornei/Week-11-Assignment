d40af4eec6886b6207d0dd9dc9c910d913232aad
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-24 12:00:02 -0600

    Added to app.js --> comments to .on('click') event and new object 'winningPositions'.

--- app.js
+++ app.js
@@ -6,9 +6,33 @@ let activeGame = true;
     Select the document object, use the .ready() method to select each cell, and add an eventlisener of click.
 */
 
+// Variable currentPlays updates number of plays that each player has made into an array.
+let currentPlays = {
+    X: [],
+    O: []
+}
+
+const winningPositions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+console.table(winningPositions)
+
+console.log(currentPlays)
+
+// document object is selected
+// .ready() is a method that is loaded before the website is displayed to the user.
 $(document).ready(function () {
-    // Select the divs with id="cell"
+    // Select the divs with class="cell"
     $('.cell').on('click', function () {
+        // this refers to the divs with class="cell" 
+        // .text() method returns the selected element and all of it's decendents as a string. 
         $(this).text(currentPlayer);
 
         currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
