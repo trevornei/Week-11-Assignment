066241f3f4ccc53a364f06f24905d8ddf885c0fa
trevornei <117675657+trevornei@users.noreply.github.com>
2023-08-25 21:01:32 -0600

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const board = $('#board');
+giconst board = $('#board');
 let currentPlayer = "X";
 let activeGame = true;
 
+// This function clears the board of any click events/ X's or O's.
 function resetGame() {
     // Select cell and use .text() to enter nothing.
     $('.cell').text("");
@@ -18,6 +19,8 @@ let currentPlays = {
     O: []
 }
 
+
+// An array of array's that contains the combinations of winning positions.
 const winningPositions = [
     [1, 2, 3],
     [4, 5, 6],
@@ -46,8 +49,6 @@ $(document).ready(function () {
         resetGame()
     })
     
-    // Tracks moves of each player
-    let moves = { 'X': [], 'O': [] };
     // Tracks the number of moves that have occured. 
     // Select the divs with class="cell"
     $('.cell').on('click', function () {
@@ -62,16 +63,19 @@ $(document).ready(function () {
             draw();
         }
         // Toggles current player with ternary operator
+        // If the click event starts with currentPlayer equal to 'X' then the next click event will have the value of currentPlayer set to 'O' and vice versa.
         currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
         numPlays++
 
     })
 })
 
+// This function enters the string Draw! into the element with the id of result.
 function draw() {
     $("#result").text('Draw!');
 }
 
+// isDraw is a funtion that returns the number of plays to 8. 
 function isDraw() {
     return numPlays === 8;
 }
@@ -81,19 +85,32 @@ function win() {
 }
 
 let isWinner = () => {
+    // numPlays uses a postfix increment to count the number of plays based on the number of clicks.
+    // The only way a player can win is if a player has clicked more than three times. 
     if (numPlays < 3) {
         return false;
     }
+    /*
+      If numPlays is greater than 3 this for loop will run.
+        - i will iterate over the lenth of winningPositions array of arrays that stores the data for all of the possible winning combinations.
 
+        - The value of win is set to true because this is the isWinner() method. 
+
+        - (Inner for loop) j iterates through each cell to check for a winning combination.
+
+        inArray() finds a value inside an array. When it finds the right occurance, the index of will be returnded. If not, the method returns -1 and the loop is terminated. 
+    */
     for (let i = 0; i < winningPositions.length; i++) {
         let win = true;
         for (let j = 0; j < winningPositions[i].length; j++) {
+            // currentPlays is an object that stores clicks made for each player, X: and O: and saves the data-index="number". 
+            // if the currentPlayers, currentPlays array does not match a winning combination, the the varaible win is set to falsey.
             if ($.inArray(winningPositions[i][j], currentPlays[currentPlayer]) < 0) {
                 win = false;
                 break;
             }
         }
-    
+        // If win is truthy then the function returns true letting the program know that currentPlayer has won the game.
         if (win) 
             return true;
     }
